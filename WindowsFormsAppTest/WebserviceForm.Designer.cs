c130da9f6240a3ec72cd17b220edfa01c7ddf502
Youri Seijkens <r0790156@student.thomasmore.be>
2022-03-03 09:11:05 +0100

    gegevens ophalen en in treeview plaatsen

--- WindowsFormsAppTest/WebserviceForm.Designer.cs
+++ WindowsFormsAppTest/WebserviceForm.Designer.cs
@@ -33,6 +33,15 @@ namespace WindowsFormsAppTest
             this.WebServiceCmbx = new System.Windows.Forms.ComboBox();
             this.TestAllBtn = new System.Windows.Forms.Button();
             this.TrVwAll = new System.Windows.Forms.TreeView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ResponseTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.WebserviceLbl = new System.Windows.Forms.Label();
+            this.textBoxWebservice = new System.Windows.Forms.TextBox();
+            this.checkBoxKraanDatabase = new System.Windows.Forms.CheckBox();
+            this.checkBoxKraanIni = new System.Windows.Forms.CheckBox();
+            this.checkBoxKraanDLL = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -49,7 +58,7 @@ namespace WindowsFormsAppTest
             this.WebServiceCmbx.FormattingEnabled = true;
             this.WebServiceCmbx.Location = new System.Drawing.Point(16, 30);
             this.WebServiceCmbx.Name = "WebServiceCmbx";
-            this.WebServiceCmbx.Size = new System.Drawing.Size(234, 21);
+            this.WebServiceCmbx.Size = new System.Drawing.Size(339, 21);
             this.WebServiceCmbx.TabIndex = 1;
             this.WebServiceCmbx.SelectedIndexChanged += new System.EventHandler(this.WebServiceCmbx_SelectedIndexChanged);
             // 
@@ -57,7 +66,7 @@ namespace WindowsFormsAppTest
             // 
             this.TestAllBtn.Location = new System.Drawing.Point(16, 57);
             this.TestAllBtn.Name = "TestAllBtn";
-            this.TestAllBtn.Size = new System.Drawing.Size(234, 23);
+            this.TestAllBtn.Size = new System.Drawing.Size(339, 23);
             this.TestAllBtn.TabIndex = 2;
             this.TestAllBtn.Text = "Test All";
             this.TestAllBtn.UseVisualStyleBackColor = true;
@@ -65,17 +74,100 @@ namespace WindowsFormsAppTest
             // 
             // TrVwAll
             // 
-            this.TrVwAll.Location = new System.Drawing.Point(280, 30);
+            this.TrVwAll.Location = new System.Drawing.Point(370, 30);
             this.TrVwAll.Name = "TrVwAll";
             this.TrVwAll.Size = new System.Drawing.Size(508, 408);
             this.TrVwAll.TabIndex = 3;
             this.TrVwAll.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.TrVwAll_AfterSelect);
+            this.TrVwAll.Click += new System.EventHandler(this.TrVwAll_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Log";
+            // 
+            // ResponseTextBox
+            // 
+            this.ResponseTextBox.Location = new System.Drawing.Point(16, 231);
+            this.ResponseTextBox.Multiline = true;
+            this.ResponseTextBox.Name = "ResponseTextBox";
+            this.ResponseTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ResponseTextBox.Size = new System.Drawing.Size(339, 207);
+            this.ResponseTextBox.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.WebserviceLbl);
+            this.groupBox1.Controls.Add(this.textBoxWebservice);
+            this.groupBox1.Controls.Add(this.checkBoxKraanDatabase);
+            this.groupBox1.Controls.Add(this.checkBoxKraanIni);
+            this.groupBox1.Controls.Add(this.checkBoxKraanDLL);
+            this.groupBox1.Location = new System.Drawing.Point(16, 86);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(339, 126);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Results";
+            // 
+            // WebserviceLbl
+            // 
+            this.WebserviceLbl.AutoSize = true;
+            this.WebserviceLbl.Location = new System.Drawing.Point(10, 17);
+            this.WebserviceLbl.Name = "WebserviceLbl";
+            this.WebserviceLbl.Size = new System.Drawing.Size(98, 13);
+            this.WebserviceLbl.TabIndex = 14;
+            this.WebserviceLbl.Text = "Webservice versie:";
+            // 
+            // textBoxWebservice
+            // 
+            this.textBoxWebservice.Location = new System.Drawing.Point(114, 14);
+            this.textBoxWebservice.Name = "textBoxWebservice";
+            this.textBoxWebservice.ReadOnly = true;
+            this.textBoxWebservice.Size = new System.Drawing.Size(217, 20);
+            this.textBoxWebservice.TabIndex = 13;
+            // 
+            // checkBoxKraanDatabase
+            // 
+            this.checkBoxKraanDatabase.AutoSize = true;
+            this.checkBoxKraanDatabase.Location = new System.Drawing.Point(8, 93);
+            this.checkBoxKraanDatabase.Name = "checkBoxKraanDatabase";
+            this.checkBoxKraanDatabase.Size = new System.Drawing.Size(100, 17);
+            this.checkBoxKraanDatabase.TabIndex = 12;
+            this.checkBoxKraanDatabase.Text = "KraanDatabase";
+            this.checkBoxKraanDatabase.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxKraanIni
+            // 
+            this.checkBoxKraanIni.AutoSize = true;
+            this.checkBoxKraanIni.Location = new System.Drawing.Point(8, 70);
+            this.checkBoxKraanIni.Name = "checkBoxKraanIni";
+            this.checkBoxKraanIni.Size = new System.Drawing.Size(65, 17);
+            this.checkBoxKraanIni.TabIndex = 11;
+            this.checkBoxKraanIni.Text = "KraanIni";
+            this.checkBoxKraanIni.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxKraanDLL
+            // 
+            this.checkBoxKraanDLL.AutoSize = true;
+            this.checkBoxKraanDLL.Location = new System.Drawing.Point(8, 47);
+            this.checkBoxKraanDLL.Name = "checkBoxKraanDLL";
+            this.checkBoxKraanDLL.Size = new System.Drawing.Size(74, 17);
+            this.checkBoxKraanDLL.TabIndex = 10;
+            this.checkBoxKraanDLL.Text = "KraanDLL";
+            this.checkBoxKraanDLL.UseVisualStyleBackColor = true;
             // 
             // WebserviceForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(890, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ResponseTextBox);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.TrVwAll);
             this.Controls.Add(this.TestAllBtn);
             this.Controls.Add(this.WebServiceCmbx);
@@ -83,6 +175,8 @@ namespace WindowsFormsAppTest
             this.Name = "WebserviceForm";
             this.Text = "WebserviceForm";
             this.Load += new System.EventHandler(this.WebserviceForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -94,5 +188,13 @@ namespace WindowsFormsAppTest
         private System.Windows.Forms.ComboBox WebServiceCmbx;
         private System.Windows.Forms.Button TestAllBtn;
         private System.Windows.Forms.TreeView TrVwAll;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ResponseTextBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label WebserviceLbl;
+        private System.Windows.Forms.TextBox textBoxWebservice;
+        private System.Windows.Forms.CheckBox checkBoxKraanDatabase;
+        private System.Windows.Forms.CheckBox checkBoxKraanIni;
+        private System.Windows.Forms.CheckBox checkBoxKraanDLL;
     }
 }
--- WindowsFormsAppTest/WebserviceForm.cs
+++ WindowsFormsAppTest/WebserviceForm.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Windows.Forms;
 
 namespace WindowsFormsAppTest
@@ -10,9 +13,22 @@ namespace WindowsFormsAppTest
         private List<UrlData> _urlDatasByForeignKey = new List<UrlData>();
 
         private int selectedWebserviceId;
+        private string url;
+        private string urlHttp = "https://wsdev.kraan.com/";
+        private string securityId = "";
+        private string webserviceVersie;
+        private string kraanDll;
+        private string kraanIni;
+        private string kraanDatabase;
+
+        private int positionKraanDll;
+        private int positionKraanIni;
+        private int positionDatabaseConnect;
+        private int positionDatabaseMelding;
 
         WebserviceTest _webserviceTest;
         UrlTest _urltest;
+        WebClient _wc;
 
 
         public WebserviceForm()
@@ -20,6 +36,7 @@ namespace WindowsFormsAppTest
             InitializeComponent();
             _webserviceTest = new WebserviceTest();
             _urltest = new UrlTest();
+            _wc = new WebClient();
             _webServiceDatas = _webserviceTest.GetWebServiceDatas(true);
         }
 
@@ -49,6 +66,9 @@ namespace WindowsFormsAppTest
             foreach (UrlData urlData in _urlDatasByForeignKey)
             {
                 TrVwAll.Nodes.Add(urlData.Name);
+                url = urlData.Name;
+                securityId = urlData.SecurityId; 
+                getWebRequest(TrVwAll.Nodes.Count);
             }
             TrVwAll.EndUpdate();
         }
@@ -56,6 +76,81 @@ namespace WindowsFormsAppTest
         private void WebServiceCmbx_SelectedIndexChanged(object sender, EventArgs e)
         {
             selectedWebserviceId = (int)WebServiceCmbx.SelectedValue;
+        }
+
+        private void getWebRequest(int laatsteNode)
+        {
+            string webRequestUrl;
+            if (securityId != string.Empty)
+            {
+                webRequestUrl = urlHttp + url + securityId;
+            }
+            else
+            {
+                webRequestUrl = urlHttp + url;
+            }
+            Uri uri = new Uri(webRequestUrl);
+            try
+            {
+                HttpWebRequest request = HttpWebRequest.Create(webRequestUrl) as HttpWebRequest;
+
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+                    int statusCode = (int)response.StatusCode;
+                    Console.WriteLine(statusCode);
+                    if (statusCode >= 100 && statusCode < 400) //Good requests
+                    {
+                        var data = _wc.DownloadString(uri);
+                        if (data.Count(d => d == '{') > 1)
+                        {
+                            int Pos1 = data.IndexOf('{');
+                            int Pos2 = data.IndexOf("}");
+                            data = data.Substring(Pos1, Pos2 - Pos1 + 1);
+                        }
+                        if (securityId == string.Empty)
+                        {
+                            positionKraanDll = data.IndexOf("KraanDLL");
+                            positionKraanIni = data.IndexOf("Kraan.ini");
+                            positionDatabaseConnect = data.IndexOf("Database connectie");
+                            positionDatabaseMelding = data.IndexOf("Database melding");
+
+                            webserviceVersie = data.Substring(0, positionKraanDll);
+                            kraanDll = data.Substring(positionKraanDll, positionKraanIni - positionKraanDll);
+                            kraanIni = data.Substring(positionKraanIni, positionDatabaseConnect - positionKraanIni);
+                            kraanDatabase = data.Substring(positionDatabaseConnect, positionDatabaseMelding - positionDatabaseConnect);
+
+                            string[] strlist = webserviceVersie.Split(':');
+                            textBoxWebservice.Text = strlist[1];
+
+                            checkBoxKraanDLL.Checked = kraanDll.Contains("True");
+                            checkBoxKraanIni.Checked = kraanIni.Contains("True");
+                            checkBoxKraanDatabase.Checked = kraanDatabase.Contains("True");
+                            ResponseTextBox.Text = data;
+                        }
+                        else
+                        {
+                            dynamic result = JObject.Parse(data);
+                            foreach (JProperty item in result)
+                            {
+                                TrVwAll.Nodes[laatsteNode - 1].Nodes.Add(item.Name + " " + item.Value);
+                            }
+                        }
+                    }
+                    else if (statusCode >= 500 && statusCode <= 510) //Server Errors
+                    {
+                        ResponseTextBox.Text = "niet goed";
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                ResponseTextBox.Text = ex.ToString();
+            }
+        }
+
+        private void TrVwAll_Click(object sender, EventArgs e)
+        {
+            //Console.WriteLine(TrVwAll.HitTest(TrVwAll.PointToClient(Cursor.Position)).Node);
         }
     }
 }
