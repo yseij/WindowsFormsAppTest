db57e5e6ab694405b3a1aa9e2a8c4e1f85815d98
yseij <r0790156@student.thomasmore.be>
2022-05-18 10:21:30 +0200

    kleine aanpassingen gedaan

--- WindowsFormsAppTest/KlantXml.cs
+++ WindowsFormsAppTest/KlantXml.cs
@@ -12,9 +12,7 @@ namespace WindowsFormsAppTest
 
         public List<Klant> GetKlanten()
         {
-            MessageBox.Show(_path);
             XDocument doc = XDocument.Load(_path);
-            MessageBox.Show("hier");
             List<Klant> klanten = new List<Klant>();
             foreach (XElement element in doc.Descendants("Klant"))
             {
